4e35aba80c6747e34ee51b728ae72be00723067d
Josiah-Canada <josiahcanada@yahoo.com>
2022-04-02 19:59:50 -0400

    added randomness

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//var upperCase = "ABCD"
+// var loweCase =
+// var specialChar =
+// var numbers =
+
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+    var charAmount =  window.prompt("How many characters do you want your passwrod");
+        if (charAmount > 8 || charAmount > 128) {
+        alert("Please select a valid response")
+        } 
+
+        console.log(charAmount);
+      var charCase = window.prompt("Do you wish to include lowercase letters, uppercase letter, and special characters?");
+        if (charCase != "yes") {
+          alert("Please select a valid reponse")
+        } else {
+          generatePassword();
+        }
+    
+      function generatePassword() {
+      var text = ""
+      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*?";
+      for (var i = 0; i < 8; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+        console.log(text);
+      }
+
+      
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+}
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -11,4 +61,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
