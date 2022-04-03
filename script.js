6c3c1fe84e08eabdd752ccb8b9b11f5ed2b1110a
Josiah-Canada <josiahcanada@yahoo.com>
2022-04-03 13:23:31 -0400

    complete

--- script.js
+++ script.js
@@ -4,75 +4,64 @@
 // var numbers =
 
 // // Assignment Code
-    var generateBtn = document.querySelector("#generate");
+var blandText = ""
+var text = ""
+var generateBtn = document.querySelector("#generate");
+    
     var charAmount =  window.prompt("How many characters do you want your passwrod");
-        if (charAmount > 8 || charAmount > 128) {
-        alert("Please select a valid response")
-        } 
-
-        console.log(charAmount);
-      var charCase = window.prompt("Do you wish to include lowercase letters, uppercase letter, and special characters?");
-        if (charCase != "yes") {
-          alert("Please select a valid reponse")
+        if (charAmount > 8 || charAmount < 128) {
+        alert("creating " + charAmount + " character password; One more thing.")
         } else {
-          generatePassword();
+          alert("Please select a valid reponse")
         }
-    
-      function generatePassword() {
-      var text = ""
-      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*?";
-
-      for (var i = 0; i < charAmount; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-        
-      }
-    
-      console.log(text);
       
-      const btn = document.getElementById("btn");
-      btn.addEventListener("click", ()=>{
-        if(btn.value === "change"){
-          btn.value = text 
+      console.log(charAmount);
+        
+      var charCase = window.prompt("Do you wish to include uppercase letter and special characters?");
+        if (charCase === "yes") {
+          coolPassword();
+        } else if (charCase === "no") {
+          boringPassword();
+        } 
+        
+      function coolPassword() {
+        //var text = ""
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*?";
+  
+        for (var i = 0; i < charAmount; i++) {
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
         }
-      })
-      
+        console.log(text);  
+  }
+        
+        
+        function boringPassword() {
+          //var blandText = ""
+          var available = "abcdefghijklmnopqrstuvwxyz";
 
-      
+          for (var i = 0; i < charAmount; i++) {
+            blandText += available.charAt(Math.floor(Math.random() * available.length));
+          }
+          console.log(blandText);
+        }
 
-      
-      
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-
-}
 
 
 
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+// (function () {
+//   var old = console.log;
+//   var logger = document.getElementById('password');
+//   console.log = function (message) {
+//     if (typeof message == 'object') {
+//         logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />';
+//     } else {
+//         logger.innerHTML += message + '<br />';
+//     }
+// }
+// })();
 
 // Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", );
